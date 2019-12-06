41330d9488349d7c495d5e4139b6f9295dd4bb08
Atin <atiagarw@cisco.com>
2019-12-06 12:00:18 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,48 @@
+#!flask/bin/python
+from flask import Flask,request
+import pymongo
+import subprocess
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["test"]
+mycol = mydb["model_inventory"]
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/api/v1/tryit', methods=['GET'])
+def try_it():
+    api_name = request.args.get('apiName')
+    print('API Name :: ' + api_name)
+
+    myquery = { "name": api_name }
+    myDoc = mycol.find_one(myquery)
+
+    result = myDoc
+    print("Result from DB :: ", result)
+
+    modelFileLocation = result['modelFileLocation']
+    inputFileLocation = result['inputFileLocation']
+
+    with open(inputFileLocation, 'r') as myfile:
+        data = myfile.readlines()
+        print("Input data :: ", data)
+    
+    cmd = 'python3 ' + modelFileLocation + ' ' + data[0].replace('\n', '') + ' ' + data[1].replace('\n', '') + ' ' + data[2].replace('\n', '')
+    print('Command running :: ', cmd)
+
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    out, err = p.communicate() 
+    decodedOut = out.decode('utf8')
+    print("Err:: ", err)
+    print("Output :: " , decodedOut)
+
+    return (decodedOut)
+   
+
+if __name__ == '__main__':
+    app.run(debug=True)
